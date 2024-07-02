476c855b607d4e7257fe44eacb92341b907eea0c
Joselito Bacolod <115162392+joshbigmouse@users.noreply.github.com>
2024-07-02 20:17:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1=6
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1=6
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice image generator
+var randomImageSource = "images/" + randomDiceImage; //random dice image
+
+var image1 = document.querySelectorAll("img")[0]; //dice 1
+
+image1.setAttribute("src", randomImageSource);
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //dice 2
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
